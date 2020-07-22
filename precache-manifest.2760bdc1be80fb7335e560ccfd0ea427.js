016375663e32f457c9ab50d1120bdd087701cbf7
Bert Madwed <dovebaer@aol.com>
2020-07-21 20:27:14 -0400

    Updates

---
+++ precache-manifest.2760bdc1be80fb7335e560ccfd0ea427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99301da41c95344aad7006018ef933a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "125f5521147c819dce9e",
+    "url": "/monsters-rolodex/static/css/main.c3869669.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "125f5521147c819dce9e",
+    "url": "/monsters-rolodex/static/js/main.b2655072.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
